312582cbcaaa8a0425c32b9b9c4d46152e138c1c
John <TPgMNMKQLre897@rs.ru>
2023-12-05 10:13:28 +0300

    Добавлен функционал редактирования страниц

--- GearShop/Controllers/Shop/ContactsController.cs
+++ GearShop/Controllers/Shop/ContactsController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GearShop.Contracts;
+using GearShop.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GearShop.Controllers.Shop
 {
 	public class ContactsController : Controller
 	{
-		public IActionResult Index()
+		private readonly IGearShopRepository _gearShopRepository;
+
+		public ContactsController(IGearShopRepository gearShopRepository)
 		{
-			return View();
+			_gearShopRepository = gearShopRepository;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			ContactsPageViewModel model = new ContactsPageViewModel();
+			model.PageContent = await _gearShopRepository.GetPageContent("ContactsPage");
+			return View(model);
 		}
 	}
 }
